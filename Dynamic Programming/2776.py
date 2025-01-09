33a2db43fd8483b6cfd96d2028e668c24bc679ae
wogml8524 <wogml8524@naver.com>
2025-01-09 13:30:12 +0900

    Create 2776.py

---
+++ Dynamic Programming/2776.py
@@ -0,0 +1,23 @@
+'''
+    2775번 "부녀회장이 될테야" 브론즈 1
+    
+    DP
+'''
+
+import sys
+
+
+T = int(sys.stdin.readline().strip())
+
+for _ in range(T):
+    K = int(sys.stdin.readline().strip()) # k층
+    N = int(sys.stdin.readline().strip()) # n호
+
+    # 0층의 사람들
+    DP = [i for i in range(1,N+1)]
+    
+    for _ in range(K): # k층까지 반복
+        for n in range(1, N): # 1부터 n-1호까지 반복
+            DP[n] += DP[n-1]
+            
+    print(DP[-1])
